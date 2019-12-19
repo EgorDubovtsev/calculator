588bf06a4758bfb9fef59033f4b8f205d8df1e1c
Egor <egot.dubovczcev@yandex.ru>
2019-12-19 12:57:06 +0400

    Revert "Закончил выполнение усложненного задания"
    This reverts commit 7e6e2c122b03bc8d60bbb05fa66df53b77744549.

--- calc/src/Main.java
+++
@@ -1,9 +0,0 @@
-import exceptions.IllegalOperation;
-import service.ConsoleInputOutputService;
-
-public class Main {
-    public static void main(String[] args) throws IllegalOperation {
-        ConsoleInputOutputService consoleInputOutputService = new ConsoleInputOutputService();
-        consoleInputOutputService.readLine();
-    }
-}
